184c3e8553d2a8da13fd9547961ae9ec2e2202e3
Hafees <abdulhafeesk@gmail.com>
2023-02-24 18:02:54 +0400

    employees api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\EmployeeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,4 +25,5 @@ Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
+    Route::apiResource('employees', EmployeeController::class);
 });
